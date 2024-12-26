4a9e23a8e3250ff0c46bbd7a788dad0ce8adf3a1
sooraj4sure <skashyap9711@gmail.com>
2024-12-26 15:34:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const port = 8080;
+
+app.listen(()=>{
+    console.log(port," port listeningg ");
+    
+})
